f2cd24fc59e44c9e40c02c8809fe477707b05ff1
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-03-04 09:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalogo/Contexto/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Contexto
+{
+    public class AppDbContext : DbContext // quando eu faço a herança, o entity é adicionado. Ela realiza a comunicação entre as entidades e o banco de dados relacional. 
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria>?Categorias { get; set; }
+        public DbSet<Produto>?Produtos { get; set; }
+    }
+}
---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo.Models
+{
+    public class Categoria
+    {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+
+        public int CategoriaId { get; set; }   // chave primária // pra identificar kda categoria
+        public string? Nome { get; set; }
+
+        public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto>?Produtos { get; set; }
+
+    }
+}
