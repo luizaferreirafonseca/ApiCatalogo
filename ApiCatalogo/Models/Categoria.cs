926d17bbdb3536b90b0825c272ecfc0643ce9c3e
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-03-06 08:10:09 -0300

    adding controllers

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiCatalogo.Models
 {
+
+    [Table("Categorias")] // indicando que quero mapear pra tabela categorias. mas ao definir o dbset eu já faço isso também.
     public class Categoria
     {
+
+
 
         public Categoria()
         {
             Produtos = new Collection<Produto>();
         }
 
-
+        [Key]
         public int CategoriaId { get; set; }   // chave primária // pra identificar kda categoria
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
 
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto>?Produtos { get; set; }
